0d29eb21d26f3bdb958dd3ba14c7e4e1f798c158
Daniel Johnson <navseeker7@gmail.com>
2015-07-16 18:49:59 -0700

    copy all font file types to public

--- mimosa-config.js
+++ mimosa-config.js
@@ -22,5 +22,14 @@ exports.config = {
   },
   minifyJS: {
     mangleNames: false
+  },
+  copy: {
+    extensions: ["js",  "css", "png", "jpg",
+      "jpeg","gif", "html","php",
+      "eot", "svg", "ttf", "woff",
+      "otf", "yaml","kml", "ico",
+      "htc", "htm", "json","txt",
+      "xml", "xsd", "map", "md",
+      "mp4", "woff2"]
   }
 }
